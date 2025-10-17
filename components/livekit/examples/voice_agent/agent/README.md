1f771163964f16a2bd0b0c6551f65d0ad5d73dfe
yuanyanhui <51470109+yuanyanhui@users.noreply.github.com>
2025-10-17 09:19:13 +0000

    add generate_token.py and update agent README

---
+++ components/livekit/examples/voice_agent/agent/README.md
@@ -0,0 +1,4 @@
+uv run python agent.py download-files
+uv run agent.py dev
+
+https://agents-playground.livekit.io/
---
+++ components/livekit/examples/voice_agent/agent/generate_token.py
@@ -0,0 +1,19 @@
+# server.py
+import os
+from livekit import api
+
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env")
+
+def getToken():
+  token = api.AccessToken(os.getenv('LIVEKIT_API_KEY'), os.getenv('LIVEKIT_API_SECRET')) \
+    .with_identity("identity") \
+    .with_name("my name") \
+    .with_grants(api.VideoGrants(
+        room_join=True,
+        room="my-room",
+    ))
+  return token.to_jwt()
+
+print(getToken())
